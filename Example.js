cc136bccade431046dcdd19352835347dcc4219e
Nguyễn Công Hậu <111746781+nguyenhau8209@users.noreply.github.com>
2023-05-13 16:45:36 +0700

    hauput len git

--- Example.js
+++ Example.js
@@ -2,3 +2,5 @@ const helloChinh = 'Xin chao Chinh nhe!';
 console.log(helloChinh);
 console.log(helloChinh + 'minh la Hau day');
 console.log(helloChinh + 'minh la Hau day');
+console.log(helloChinh + 'minh la Hau day');
+console.log(helloChinh + 'minh la Hau day');
